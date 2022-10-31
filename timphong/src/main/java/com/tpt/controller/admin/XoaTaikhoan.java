623f881db66bd128128d567e77e939557fdd0802
cwmTi3n <hokimtien2002@gmail.com>
2022-11-01 00:50:52 +0700

    Create XoaTaikhoan.java

---
+++ timphong/src/main/java/com/tpt/controller/admin/XoaTaikhoan.java
@@ -0,0 +1,39 @@
+package com.tpt.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tpt.dao.ITaikhoanDao;
+import com.tpt.service.ITaikhoanService;
+import com.tpt.service.impl.TaikhoanServiceImpl;
+
+@WebServlet(urlPatterns = {"/admin/xoa-taikhoan"})
+public class XoaTaikhoan extends HttpServlet
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	ITaikhoanService taikhoanService = new TaikhoanServiceImpl();
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		req.setCharacterEncoding("utf-8");
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html");
+		String id_tkString = req.getParameter("id_tk");
+		int id_tk;
+		if(id_tkString != null)
+		{
+			id_tk = Integer.parseInt(id_tkString);
+			System.out.println(taikhoanService.deleteTaikhoan(id_tk));
+		}
+		resp.sendRedirect(req.getContextPath() + "/admin/list-taikhoan");
+	}
+}
