53d0c4254bb6c667cfcf825bfa43d32125d146e1
cwmTi3n <hokimtien2002@gmail.com>
2022-11-01 00:50:47 +0700

    Create LoaiphongDaoImpl.java

---
+++ timphong/src/main/java/com/tpt/dao/impl/LoaiphongDaoImpl.java
@@ -0,0 +1,38 @@
+package com.tpt.dao.impl;
+
+import com.tpt.connection.DBConnection;
+import com.tpt.dao.ILoaiphongDao;
+import com.tpt.model.Loaiphong;
+
+public class LoaiphongDaoImpl extends DBConnection implements ILoaiphongDao
+{
+
+	@Override
+	public Loaiphong getLoaiphong()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertLoaiphong()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteLoaiphong()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean editLoaiphong()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
