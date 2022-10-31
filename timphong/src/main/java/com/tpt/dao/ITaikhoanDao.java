c81f7e5138f3dac60ff7b4dd7f350008097c80d2
cwmTi3n <hokimtien2002@gmail.com>
2022-10-31 23:59:26 +0700

    Create ITaikhoanDao.java

---
+++ timphong/src/main/java/com/tpt/dao/ITaikhoanDao.java
@@ -0,0 +1,16 @@
+package com.tpt.dao;
+
+import java.util.List;
+
+import com.tpt.model.Taikhoan;
+
+public interface ITaikhoanDao
+{
+	public List<Taikhoan> getAll();
+	public List<Taikhoan> getAllUser();
+	public List<Taikhoan> getAllSeller();
+	public Taikhoan getTaikhoan(int id_tk);
+	public boolean insertTaikhoan(Taikhoan taikhoan);
+	public boolean deleteTaikhoan(int id_tk);
+	public boolean editTaikhoan(Taikhoan taikhoan);
+}
