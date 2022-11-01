23727355184367794b5aa90c9c984bf518563e55
cwmTi3n <hokimtien2002@gmail.com>
2022-11-01 17:58:44 +0700

    Update IPhongService.java

--- timphong/src/main/java/com/tpt/service/IPhongService.java
+++ timphong/src/main/java/com/tpt/service/IPhongService.java
@@ -7,6 +7,7 @@ import com.tpt.model.Phong;
 public interface IPhongService
 {
 	public List<Phong> getPhongSeller(int id_tk);
+	public List<Phong> getPhongLoaiphong(int id_lp);
 	public Phong getPhong(int id_p);
 	public boolean editPhong(Phong newPhong);
 	public boolean insertPhong(Phong phong);
