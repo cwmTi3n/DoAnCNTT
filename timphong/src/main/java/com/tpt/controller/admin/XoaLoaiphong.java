91c4342e03c52d99ff0cc12aeb63a17912e5f4ce
cwmTi3n <hokimtien2002@gmail.com>
2022-11-01 17:58:19 +0700

    Create XoaLoaiphong.java

---
+++ timphong/src/main/java/com/tpt/controller/admin/XoaLoaiphong.java
@@ -0,0 +1,37 @@
+package com.tpt.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tpt.service.ILoaiphongService;
+import com.tpt.service.impl.LoaiphongServiceImpl;
+
+@WebServlet(urlPatterns = {"/xoa-loaiphong"})
+public class XoaLoaiphong extends HttpServlet
+{
+
+	/**
+	 * 
+	 */
+	ILoaiphongService loaiphongService = new LoaiphongServiceImpl();
+	private static final long serialVersionUID = 1L; 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html");
+		
+		String id_lpString = req.getParameter("id_lp");
+		
+		int id_lp = Integer.parseInt(id_lpString);
+		
+		loaiphongService.deleteLoaiphong(id_lp);
+		
+		resp.sendRedirect(req.getContextPath() + "/admin/list-loaiphong");
+	}
+}
