246ad6b50f7caeb8682c5d0ebc135737f9506edb
cwmTi3n <hokimtien2002@gmail.com>
2022-11-01 15:58:15 +0700

    Update ILoaiphongDao.java

--- timphong/src/main/java/com/tpt/dao/ILoaiphongDao.java
+++ timphong/src/main/java/com/tpt/dao/ILoaiphongDao.java
@@ -1,13 +1,16 @@
 package com.tpt.dao;
 
+import java.util.List;
+
 import org.apache.el.parser.BooleanNode;
 
 import com.tpt.model.Loaiphong;
 
 public interface ILoaiphongDao
 {
-	public Loaiphong getLoaiphong();
-	public boolean insertLoaiphong();
+	public List<Loaiphong> getAll();
+	public Loaiphong getLoaiphong(int id_lp);
+	public boolean insertLoaiphong(Loaiphong lp);
 	public boolean deleteLoaiphong();
 	public boolean editLoaiphong();
 }
