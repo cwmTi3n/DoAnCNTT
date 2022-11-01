9b560169585c457f6ef19e4ad3f8c6ccb4bacfce
cwmTi3n <hokimtien2002@gmail.com>
2022-11-01 17:59:38 +0700

    Update ILoaiphongService.java

--- timphong/src/main/java/com/tpt/service/ILoaiphongService.java
+++ timphong/src/main/java/com/tpt/service/ILoaiphongService.java
@@ -8,4 +8,6 @@ public interface ILoaiphongService
 {
 	public boolean insertLoaiphong(Loaiphong lp);
 	public List<Loaiphong> getAll();
+	public Loaiphong getLoaiphong(int id_lp);
+	public boolean deleteLoaiphong(int id_lp);
 }
