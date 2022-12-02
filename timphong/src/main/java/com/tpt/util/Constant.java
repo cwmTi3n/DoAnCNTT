4eee6a4f46f908396452336d0a71a313c6b79cab
cwmTi3n <hokimtien2002@gmail.com>
2022-12-02 10:41:27 +0700

    Commit

--- timphong/src/main/java/com/tpt/util/Constant.java
+++ timphong/src/main/java/com/tpt/util/Constant.java
@@ -2,11 +2,9 @@ package com.tpt.util;
 
 public class Constant
 {
-
-	//public static final String DIR="F://upload";
-	public static final String[] thutu = {"phong.id_p", "phong.ngay", "phong.ngay DESC", "phong.gia", "phong.gia DESC", "danhgia", "danhgia DESC"};
+	public static final String[] thutu = {"phong.id_p", "phong.ngaydang", "phong.ngaydang DESC", "phong.gia", "phong.gia DESC", "danhgia", "danhgia DESC"};
 	public static final String[] boloc = {"and id_lp = ? ", "and tinhThanhPhoId = ? ", "and quanHuyenId = ? ", "and id_x = ? ", "and songuoi = ?"};
-	public static final String DIR="F://upload";
+	public static final String DIR="E://upload";
 	public static final int SoHinh = 3;
 	public static final int BoLoc = 5;
 	public static final String subMailVerify = "Mã xác thực đăng ký tài khoản Find Room";
